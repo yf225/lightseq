ccdf6c646004ce20f28cf394d54dd6acbfec1eaa
Will Feng (DPER) <willfeng@fb.com>
2021-11-29 19:49:23 -0800

    update

--- lightseq/training/__init__.py
+++ lightseq/training/__init__.py
@@ -7,7 +7,7 @@ from lightseq.training.ops.pytorch.transformer_encoder_layer import (
 from lightseq.training.ops.pytorch.transformer_decoder_layer import (
     LSTransformerDecoderLayer,
 )
-from lightseq.training.ops.pytorch.transformer import LSTransformer
+from lightseq.training.ops.pytorch.transformer import LSTransformer, LSVisionTransformer
 from lightseq.training.ops.pytorch.cross_entropy_layer import LSCrossEntropyLayer
 from lightseq.training.ops.pytorch.adam import LSAdam
 from lightseq.training.ops.pytorch.export import (
